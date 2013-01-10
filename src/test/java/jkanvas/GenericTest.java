cd9d7a723e7ecb7ff95a467fb3d3297fb6f1a67e
Josua Krause <josua.krause@googlemail.com>
2013-01-10 10:03:02 +0100

    generic test

--- src/test/java/jkanvas/GenericTest.java
+++ src/test/java/jkanvas/GenericTest.java
@@ -4,8 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * A generic test. Always succeeds.
+ * 
+ * @author Joschi <josua.krause@googlemail.com>
+ */
 public class GenericTest {
 
+  /** Always succeeding test. */
   @Test
   public void foo() {
     assertFalse(false);
