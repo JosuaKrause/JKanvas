89b6bfc25034345d8e52159ac011b34c614c2290
Josua Krause <josua.krause@googlemail.com>
2013-01-11 13:47:17 +0100

    fitting rectangles into each other

--- src/main/java/jkanvas/util/PaintUtil.java
+++ src/main/java/jkanvas/util/PaintUtil.java
@@ -50,4 +50,49 @@ public final class PaintUtil {
         rect.getWidth() + p2, rect.getHeight() + p2);
   }
 
+  /**
+   * Scales a rectangle so that the center point stays the same.
+   * 
+   * @param rect The rectangle to scale.
+   * @param scale The scaling factor.
+   * @return The scaled rectangle.
+   */
+  public static Rectangle2D scaleCenter(final Rectangle2D rect, final double scale) {
+    final double w = rect.getWidth() * scale;
+    final double h = rect.getHeight() * scale;
+    return new Rectangle2D.Double(rect.getCenterX() - w * 0.5, rect.getCenterY() - h
+        * 0.5, w, h);
+  }
+
+  /**
+   * Fits a rectangle into the given rectangle. The aspect ratio of the
+   * resulting rectangle is given by <code>w</code> and <code>h</code>.
+   * 
+   * @param rect The rectangle.
+   * @param w The width for defining the aspect ratio.
+   * @param h The height for defining the aspect ratio.
+   * @return A rectangle with the given aspect ratio that fits into the given
+   *         rectangle.
+   */
+  public static Rectangle2D fitInto(final Rectangle2D rect, final double w, final double h) {
+    final double verWidth = w * rect.getHeight() / h;
+    final boolean vertical = verWidth <= rect.getWidth();
+    final double px;
+    final double py;
+    final double nw;
+    final double nh;
+    if(vertical) {
+      nw = verWidth;
+      nh = rect.getHeight();
+      px = (rect.getWidth() - nw) * 0.5;
+      py = 0;
+    } else {
+      nw = rect.getWidth();
+      nh = h * rect.getWidth() / w;
+      px = 0;
+      py = (rect.getHeight() - nh) * 0.5;
+    }
+    return new Rectangle2D.Double(rect.getX() + px, rect.getY() + py, nw, nh);
+  }
+
 }
---
+++ src/test/java/jkanvas/util/PaintUtilTest.java
@@ -0,0 +1,85 @@
+package jkanvas.util;
+
+import static org.junit.Assert.*;
+
+import java.awt.geom.Rectangle2D;
+
+import org.junit.Test;
+
+/**
+ * Tests some paint utility methods.
+ * 
+ * @author Joschi <josua.krause@googlemail.com>
+ */
+public class PaintUtilTest {
+
+  /**
+   * Whether the two rectangles are the same.
+   * 
+   * @param a The expected rectangle.
+   * @param b The actual rectangle.
+   */
+  private static void sameRect(final Rectangle2D a, final Rectangle2D b) {
+    assertEquals(a.getX(), b.getX(), Math.ulp(a.getX()));
+    assertEquals(a.getY(), b.getY(), Math.ulp(a.getY()));
+    assertEquals(a.getWidth(), b.getWidth(), Math.ulp(a.getWidth()));
+    assertEquals(a.getHeight(), b.getHeight(), Math.ulp(a.getHeight()));
+  }
+
+  /** Tests the method {@link PaintUtil#scaleCenter(Rectangle2D, double)}. */
+  @Test
+  public void zoomRectTest() {
+    final Rectangle2D r = new Rectangle2D.Double(1, 2, 4, 6);
+    final Rectangle2D r0p5 = new Rectangle2D.Double(2, 3.5, 2, 3);
+    final Rectangle2D r2p0 = new Rectangle2D.Double(-1, -1, 8, 12);
+    sameRect(r0p5, PaintUtil.scaleCenter(r, 0.5));
+    sameRect(r2p0, PaintUtil.scaleCenter(r, 2.0));
+  }
+
+  /** Tests the method {@link PaintUtil#fitInto(Rectangle2D, double, double)}. */
+  @Test
+  public void fitIntoTest() {
+    // h > w
+    final Rectangle2D outer0 = new Rectangle2D.Double(1, 2, 4, 6);
+    final Rectangle2D inner0h = new Rectangle2D.Double(1, 4, 4, 2);
+    sameRect(inner0h, PaintUtil.fitInto(outer0, 4, 2));
+    sameRect(inner0h, PaintUtil.fitInto(outer0, 8, 4));
+    sameRect(inner0h, PaintUtil.fitInto(outer0, 2, 1));
+    final Rectangle2D inner0v = new Rectangle2D.Double(2, 2, 2, 6);
+    sameRect(inner0v, PaintUtil.fitInto(outer0, 2, 6));
+    sameRect(inner0v, PaintUtil.fitInto(outer0, 4, 12));
+    sameRect(inner0v, PaintUtil.fitInto(outer0, 1, 3));
+    final Rectangle2D inner0e = new Rectangle2D.Double(1, 2, 4, 6);
+    sameRect(inner0e, PaintUtil.fitInto(outer0, 4, 6));
+    sameRect(inner0e, PaintUtil.fitInto(outer0, 2, 3));
+    sameRect(inner0e, PaintUtil.fitInto(outer0, 8, 12));
+    final Rectangle2D inner0q = new Rectangle2D.Double(1, 3, 4, 4);
+    sameRect(inner0q, PaintUtil.fitInto(outer0, 4, 4));
+    sameRect(inner0q, PaintUtil.fitInto(outer0, 1, 1));
+    // h < w
+    final Rectangle2D outer1 = new Rectangle2D.Double(1, 2, 6, 4);
+    final Rectangle2D inner1h = new Rectangle2D.Double(3, 2, 2, 4);
+    sameRect(inner1h, PaintUtil.fitInto(outer1, 2, 4));
+    sameRect(inner1h, PaintUtil.fitInto(outer1, 4, 8));
+    sameRect(inner1h, PaintUtil.fitInto(outer1, 1, 2));
+    final Rectangle2D inner1v = new Rectangle2D.Double(1, 3, 6, 2);
+    sameRect(inner1v, PaintUtil.fitInto(outer1, 6, 2));
+    sameRect(inner1v, PaintUtil.fitInto(outer1, 12, 4));
+    sameRect(inner1v, PaintUtil.fitInto(outer1, 3, 1));
+    final Rectangle2D inner1e = new Rectangle2D.Double(1, 2, 6, 4);
+    sameRect(inner1e, PaintUtil.fitInto(outer1, 6, 4));
+    sameRect(inner1e, PaintUtil.fitInto(outer1, 3, 2));
+    sameRect(inner1e, PaintUtil.fitInto(outer1, 12, 8));
+    final Rectangle2D inner1q = new Rectangle2D.Double(2, 2, 4, 4);
+    sameRect(inner1q, PaintUtil.fitInto(outer1, 4, 4));
+    sameRect(inner1q, PaintUtil.fitInto(outer1, 1, 1));
+    // h == w
+    final Rectangle2D outer2 = new Rectangle2D.Double(1, 1, 2, 2);
+    sameRect(new Rectangle2D.Double(1.5, 1, 1, 2), PaintUtil.fitInto(outer2, 2, 4));
+    sameRect(new Rectangle2D.Double(1, 1.5, 2, 1), PaintUtil.fitInto(outer2, 4, 2));
+    sameRect(outer2, PaintUtil.fitInto(outer2, 1, 1));
+    sameRect(outer2, PaintUtil.fitInto(outer2, 3, 3));
+    sameRect(outer2, PaintUtil.fitInto(outer2, 4, 4));
+  }
+
+}
