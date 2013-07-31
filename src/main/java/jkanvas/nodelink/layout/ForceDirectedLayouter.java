2c324df75ac56bd665cb14c4fff2b21f53c4c0ec
Josua Krause <josua.krause@googlemail.com>
2013-07-31 17:26:52 +0200

    better force directed layouter

--- src/main/java/jkanvas/nodelink/layout/ForceDirectedLayouter.java
+++ src/main/java/jkanvas/nodelink/layout/ForceDirectedLayouter.java
@@ -3,8 +3,8 @@ package jkanvas.nodelink.layout;
 import static jkanvas.util.VecUtil.*;
 
 import java.awt.geom.Point2D;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 import jkanvas.animation.AnimatedPosition;
 import jkanvas.animation.AnimationTiming;
@@ -21,10 +21,8 @@ public class ForceDirectedLayouter<T extends AnimatedPosition> extends
 
   /** The preferred length of an edge. */
   private double preferredLength = 200;
-  /** The spring factor. */
-  private double factor = 0.2;
   /** The movement decay. */
-  private double decay = 0.3;
+  private double decay = 3;
 
   /** Creates a force directed layouter. */
   public ForceDirectedLayouter() {
@@ -37,6 +35,8 @@ public class ForceDirectedLayouter<T extends AnimatedPosition> extends
    * @param preferredLength The preferred length of an edge.
    */
   public void setPreferredLength(final double preferredLength) {
+    if(preferredLength < 0 || preferredLength * preferredLength <= MIN_MOVEMENT_SQ) throw new IllegalArgumentException(
+        "Illegal preferred length: " + preferredLength);
     this.preferredLength = preferredLength;
   }
 
@@ -52,25 +52,8 @@ public class ForceDirectedLayouter<T extends AnimatedPosition> extends
   /**
    * Setter.
    * 
-   * @param factor The spring constant.
-   */
-  public void setFactor(final double factor) {
-    this.factor = factor;
-  }
-
-  /**
-   * Getter.
-   * 
-   * @return The spring constant.
-   */
-  public double getFactor() {
-    return factor;
-  }
-
-  /**
-   * Setter.
-   * 
-   * @param decay The decay of movement.
+   * @param decay The decay of movement. If the decay is <code>&lt;= 0</code>
+   *          the movement is not gradually slowed down.
    */
   public void setDecay(final double decay) {
     this.decay = decay;
@@ -79,7 +62,8 @@ public class ForceDirectedLayouter<T extends AnimatedPosition> extends
   /**
    * Getter.
    * 
-   * @return The decay of movement.
+   * @return The decay of movement.If the decay is <code>&lt;= 0</code> the
+   *         movement is not gradually slowed down.
    */
   public double getDecay() {
     return decay;
@@ -97,84 +81,59 @@ public class ForceDirectedLayouter<T extends AnimatedPosition> extends
     super.setTiming(timing);
   }
 
-  /** The list of node velocities. */
-  private final List<Point2D> velocity = new ArrayList<>();
-
-  @Override
-  public void deregister() {
-    super.deregister();
-    velocity.clear();
-  }
-
   /** The current decay. */
   private double curDecay;
 
-  /** The minimal movement. */
-  public static double MIN_MOVEMENT = 1e-9;
+  /** The squared minimal movement. */
+  public static double MIN_MOVEMENT_SQ = 1e-4;
 
   @Override
   public void layout(final boolean deregisterOnEnd) {
-    velocity.clear();
-    curDecay = decay;
+    curDecay = decay > 0 ? decay : 0;
     super.layout(deregisterOnEnd);
   }
 
   @Override
   protected boolean doLayout(final NodeLinkView<T> view) {
+    final double pl = preferredLength;
+    final double plSq = pl * pl;
     final int count = view.nodeCount();
-    while(velocity.size() < count) {
-      velocity.add(new Point2D.Double());
-    }
-    Point2D all = new Point2D.Double();
+    boolean chg = false;
     for(int n = 0; n < count; ++n) {
       boolean needsMovement = false;
       final T node = view.getNode(n);
       final Point2D pos = node.getPos();
       Point2D force = new Point2D.Double();
       for(int e = 0; e < count; ++e) {
+        if(n == e) {
+          continue;
+        }
         final T other = view.getNode(e);
         final Point2D diff = subVec(pos, other.getPos());
         final double lenSq = getLengthSq(diff);
-        if(view.areConnected(n, e) || lenSq < preferredLength * preferredLength) {
-          if(lenSq < MIN_MOVEMENT) {
-            needsMovement = true;
-          } else {
-            force = addVec(force, spring(diff));
-          }
+        if(lenSq < MIN_MOVEMENT_SQ) {
+          needsMovement = true;
+        } else {
+          final double spring = view.areConnected(n, e) ? Math.sqrt(lenSq) / plSq : 0;
+          final double rep = pl / lenSq;
+          force = addVec(force, mulVec(diff, rep - spring));
         }
       }
-      Point2D move = velocity.get(n);
-      if(needsMovement && force.getX() < MIN_MOVEMENT && force.getY() < MIN_MOVEMENT) {
-        final double x = Math.random() - 0.5;
-        final double y = Math.random() - 0.5;
-        force = new Point2D.Double(x * preferredLength, y * preferredLength);
+      if(curDecay > 0) {
+        force = mulVec(force, curDecay);
       }
-      move = mulVec(addVec(move, force), curDecay);
-      all = addVec(all, move);
-      velocity.set(n, move);
+      if(needsMovement && getLengthSq(force) < MIN_MOVEMENT_SQ) {
+        final Random r = ThreadLocalRandom.current();
+        final double x = r.nextGaussian();
+        final double y = r.nextGaussian();
+        force = new Point2D.Double(x * pl, y * pl);
+      }
+      chg = setPosition(node, addVec(pos, force)) || chg;
     }
-    all = mulVec(all, 1.0 / count);
-    boolean chg = false;
-    for(int n = 0; n < count; ++n) {
-      final T node = view.getNode(n);
-      final Point2D pos = node.getPos();
-      final Point2D move = subVec(velocity.get(n), all);
-      velocity.set(n, move);
-      chg = setPosition(node, addVec(pos, move)) || chg;
+    if(curDecay > 0) {
+      curDecay *= 0.99;
     }
-    curDecay *= 0.99;
     return chg;
-  }
-
-  /**
-   * Computes the spring acceleration for the given difference.
-   * 
-   * @param diff The difference vector of two nodes.
-   * @return The acceleration.
-   */
-  private Point2D spring(final Point2D diff) {
-    final double len = getLength(diff) - preferredLength;
-    return setLength(diff, -len * factor);
   }
 
 }
