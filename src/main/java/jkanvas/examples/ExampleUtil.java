e9d8c10695797bb29dfc7ca3df36d817006196e3
Josua Krause <josua.krause@gmail.com>
2014-01-29 17:36:46 -0500

    fixing tooltip bug for MAC fullscreen

--- src/main/java/jkanvas/examples/ExampleUtil.java
+++ src/main/java/jkanvas/examples/ExampleUtil.java
@@ -1,7 +1,10 @@
 package jkanvas.examples;
 
 import java.awt.Color;
+import java.awt.Frame;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Objects;
 
 import javax.swing.JFrame;
@@ -104,6 +107,24 @@ public final class ExampleUtil {
     frame.setLocationRelativeTo(null);
     frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
     frame.setVisible(true);
+    final WindowAdapter wnd = new WindowAdapter() {
+
+      @Override
+      public void windowStateChanged(final WindowEvent e) {
+        // fix for not being able to get some mouse events
+        // on a MAC when extending window -- doesn't hurt for other OSes
+        if((frame.getExtendedState() & Frame.MAXIMIZED_BOTH) == 0) return;
+        frame.setBounds(frame.getGraphicsConfiguration().getBounds());
+      }
+
+      @Override
+      public void windowClosed(final WindowEvent e) {
+        c.dispose();
+      }
+
+    };
+    frame.addWindowListener(wnd);
+    frame.addWindowStateListener(wnd);
     if(reset) {
       c.postMessage(canvasId + "#reset", 0);
     }
